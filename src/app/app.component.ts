2a87ebc942e4bb8da96fd39346774077ac435c79
paula <paulamrtinez95@gmail.com>
2019-05-07 10:51:11 -0500

    operaciones de suma y resta para dos números

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemploGithub';
+  numero1:Number=80;
+  numero2:Number=30;
+  resultado:Number;
+  resultado2:Number;
+
+
+  constructor(){
+this.sumar();
+this.restar();
+
+  }
+
+  public sumar():void{
+   this.resultado=this.numero1.valueOf()+this.numero2.valueOf();
+
+  }
+
+  public restar():void{
+    this.resultado2=this.numero1.valueOf()-this.numero2.valueOf();
+   }
+
 }
